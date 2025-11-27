1001336fafdef90889d561e5fbd9ab9ecc103752
tngtng <della.kimko@gmail.com>
2025-11-27 17:29:18 +0900

    initial commit

---
+++ MacUninstaller/MacUninstallerApp.swift
@@ -0,0 +1,32 @@
+//
+//  MacUninstallerApp.swift
+//  MacUninstaller
+//
+//  Created by tngtng on 11/27/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MacUninstallerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MacUninstallerTests/MacUninstallerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacUninstallerTests.swift
+//  MacUninstallerTests
+//
+//  Created by tngtng on 11/27/25.
+//
+
+import Testing
+@testable import MacUninstaller
+
+struct MacUninstallerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
